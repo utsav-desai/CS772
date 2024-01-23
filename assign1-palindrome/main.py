5bc836388370b72462ce699b1009e19ce2422962
Ashutosh <ashumulchandani8@gmail.com>
2024-01-23 23:55:53 +0530

    dhsfal

--- assign1-palindrome/main.py
+++ assign1-palindrome/main.py
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 # this is modified line 1 by utsav
+=======
+# this is line 1 by utsav Ccrtyuiouyah
+>>>>>>> d44e4da (dhsfal)
 
-# this is also line 2 by 16se
+# this is also line 2 by LIYRVJRIJTB1209384
